fbc22ff46db75496358490f4125f457e118c57c1
Humberto Ribeiro <humbertoemaus@gmail.com>
2024-11-12 21:01:24 -0300

    componente alerte com o sweetalert2

--- resources/js/Components/Alert/AlertMessage.jsx
+++ resources/js/Components/Alert/AlertMessage.jsx
@@ -1,22 +1,28 @@
+import { useEffect } from "react";
+import Swal from "sweetalert2";
+
 export default function AlertMessage({ message }) {
     const hasSuccess = message.success ? true : false;
     const hasError = message.error ? true : false;
 
-    if (!hasSuccess && !hasError) return null;
+    useEffect(() => {
+        if (hasSuccess) {
+            Swal.fire({
+                title: "Sucesso!",
+                text: message.success,
+                icon: "success",
+                confirmButtonColor: "#22c55e",
+            });
+        }
+        if (hasError) {
+            Swal.fire({
+                title: "Erro!",
+                text: message.error,
+                icon: "error",
+                confirmButtonColor: "#ef4444",
+            });
+        }
+    }, [message, hasSuccess, hasError]);
 
-    const alertStyle = hasSuccess ? {
-        bg: 'bg-green-50 dark:bg-gray-800',
-        text: 'text-green-800 dark:text-green-400',
-        content: message.success,
-    } : {
-        bg: 'bg-red-50 dark:bg-gray-800',
-        text: 'text-red-800 dark:text-red-400',
-        content: message.error,
-    }
-
-    return (
-        <div className={`p-3 m-3 text-sm rounded-lg ${alertStyle.text} ${alertStyle.bg}`}>
-            <span>{alertStyle.content}</span>
-        </div>
-    );
+    return null;
 }
